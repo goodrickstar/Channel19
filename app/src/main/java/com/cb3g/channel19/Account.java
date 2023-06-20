59590d5edd525dcbd6ed09c93d33492eb9b086ef
MSI <3gcb19@gmail.com>
2023-06-19 19:24:37 -0500

    MSI

--- app/src/main/java/com/cb3g/channel19/Account.java
+++ app/src/main/java/com/cb3g/channel19/Account.java
@@ -11,7 +11,7 @@ import androidx.fragment.app.Fragment;
 import com.example.android.multidex.myapplication.R;
 
 import org.jetbrains.annotations.NotNull;
-
+//TESTER
 public class Account extends Fragment {
     private TextView status;
 
